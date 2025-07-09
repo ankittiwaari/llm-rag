ccff5553d9d5fcf55c77723bee5ec9516509adbb
Ankit Tiwari <ankittiwaari@gmail.com>
2025-07-09 19:14:11 +0530

    Update db.py

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from langchain_ollama import OllamaEmbeddings
 embeddings = OllamaEmbeddings(model="mxbai-embed-large")
 
 vector_store = Chroma(
-    collection_name="onecom_collection",
+    collection_name="my_collection",
     embedding_function=embeddings,
     persist_directory="./chroma_langchain_db",  # Where to save data locally, remove if not necessary
 )   
